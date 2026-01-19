d552ae7919b62ed4d281bcef5937d1fe129dcfd0
Aman Githala <221806646+Aman-Git-hala@users.noreply.github.com>
2026-01-19 11:48:33 +0530

    fix: update backend URL to Railway

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // src/config.js
 export const API_URL = import.meta.env.PROD
-    ? "https://break-and-enter-backend.onrender.com"
+    ? "https://web-production-1212d.up.railway.app/"
     : "http://127.0.0.1:5000";
